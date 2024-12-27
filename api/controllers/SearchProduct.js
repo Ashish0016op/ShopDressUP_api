ff566635c4b55764d8002ec27bbd49953dcf469d
Ashish kumar <115461916+Ashish0016op@users.noreply.github.com>
2024-12-28 01:13:18 +0530

    Update SearchProduct.js

--- api/controllers/SearchProduct.js
+++ api/controllers/SearchProduct.js
@@ -1,14 +1,21 @@
 
 const Product=require('../model/products');
 
-exports.getProductBySearch=async(req,res,next)=>{
+exports.getProductBySearch = async (req, res, next) => {
     try {
         const { keyword } = req.query;
+        if (!keyword) {
+            return res.status(400).json({ error: 'Keyword is required for search' });
+        }
         const products = await Product.find({ product_title: { $regex: keyword, $options: 'i' } }).populate('product_photos');
-
+        
+        if (products.length === 0) {
+            return res.status(404).json({ message: 'No products found' });
+        }
+        
         res.json(products);
     } catch (err) {
-        console.log("error");
-        res.status(400).json({ error: err.message });
+        console.error("Error occurred while searching for products:", err);
+        res.status(500).json({ error: 'Internal server error' });
     }
 }
